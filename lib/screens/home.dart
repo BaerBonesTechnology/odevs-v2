1fcb33eef05399f2fe4e3d3beea36492fbf7b20a
BaerBonesTech <103000821+JoannJamie@users.noreply.github.com>
2023-02-18 19:37:08 -0500

    Added firestore functionality for any data required from database [Events, Jobs, Blog]

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,3 +19,5 @@ class _HomeScreenState extends State<HomeScreen> {
     ]);
   }
 }
+
+
--- lib/viewmodels/home_viewmodel.dart
+++ lib/viewmodels/home_viewmodel.dart
@@ -1,4 +1,28 @@
 //TODO: get events upcoming from MeetUp API
 //TODO: get job postings from firebase database
 //TODO: get blog posts from firebase database
-//TODO: get sponsor information from firebase databaseNFC Payments 
+//TODO: get sponsor information from firebase databaseNFC Payments
+
+import 'package:ODevs/util/database_helper.dart';
+
+import '../models/job_post.dart';
+
+class HomeViewModel {
+  /// A function that retrieves job post data from firebase firestore
+  /// and returns a list of job posts
+
+  final DatabaseHelper db = DatabaseHelper();
+  late List<JobPost> jobPosts;
+
+  HomeViewModel() {
+    getJobPosts();
+  }
+
+  List<JobPost> getJobPosts() {
+    db.getJobPosts().then((value) {
+      jobPosts = value;
+    });
+
+    return jobPosts;
+  }
+}
